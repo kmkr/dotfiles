b1fb506138afde5def7240efd80d26364ca9c49c
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-01-10 14:10:39 +0100

    Improve zsh

--- configure.sh
+++ configure.sh
@@ -29,6 +29,10 @@ echo "Configuring shell (zsh)"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /usr/bin/zsh
 cp zsh/zshrc ~/.zshrc
+echo "Which color do you want on prompt? (turquoise / orange / purple / hotpink /limegreen)"
+read prompt_color
+cp zsh/half-life-km.zsh-theme ~/.oh-my-zsh/themes/
+sed -i s/PROMPT_COLOR/"$prompt_color"/ ~/.oh-my-zsh/themes/half-life-km.zsh-theme
 
 echo
 
